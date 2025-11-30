faf663cb93a63129f5fa189777661dbf8df33224
Samuel Dandy <sam.dandy@tigers.com>
2025-11-29 22:11:57 -0600

    8080

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/samdandy/go_crypto
+module github.com/samdandy/go_encrypt_api
 
 go 1.24.0
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/samdandy/go_crypto v0.0.0-20251130040541-59ede60283b4
+)
--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/samdandy/go_crypto/internal/handlers"
+	"github.com/samdandy/go_encrypt_api/internal/handlers"
 )
 
 func main() {
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
-	fmt.Println("Starting server on :8081")
-	err := http.ListenAndServe(":8081", r)
+	fmt.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
